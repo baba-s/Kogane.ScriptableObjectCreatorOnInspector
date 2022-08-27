362ce6bb2dc74aad6eadeae3889c5647f80b326f
baba-s <shotababa1206@gmail.com>
2022-08-27 18:05:30 +0900

    add

---
+++ Editor/ScriptableObjectCreatorOnInspector.cs
@@ -0,0 +1,54 @@
+﻿using UnityEditor;
+using UnityEngine;
+
+namespace Kogane.Internal
+{
+    [InitializeOnLoad]
+    internal static class ScriptableObjectCreatorOnInspector
+    {
+        static ScriptableObjectCreatorOnInspector()
+        {
+            Editor.finishedDefaultHeaderGUI -= OnGUI;
+            Editor.finishedDefaultHeaderGUI += OnGUI;
+        }
+
+        private static void OnGUI( Editor editor )
+        {
+            if ( !EditorUtility.IsPersistent( editor.target ) ) return;
+
+            var assetPath = AssetDatabase.GetAssetPath( editor.target );
+            var type      = AssetDatabase.GetMainAssetTypeAtPath( assetPath );
+
+            if ( type != typeof( MonoScript ) ) return;
+
+            var monoScript = AssetDatabase.LoadAssetAtPath<MonoScript>( assetPath );
+
+            if ( monoScript == null ) return;
+
+            var classType = monoScript.GetClass();
+
+            if ( classType == null ) return;
+
+            var isScriptableObject = classType.IsSubclassOf( typeof( ScriptableObject ) );
+
+            if ( !isScriptableObject ) return;
+            if ( !GUILayout.Button( "Create Asset" ) ) return;
+
+            var fullPath = EditorUtility.SaveFilePanel
+            (
+                title: "",
+                directory: "Assets",
+                defaultName: classType.Name,
+                extension: "asset"
+            );
+
+            if ( string.IsNullOrWhiteSpace( fullPath ) ) return;
+
+            var relativePath = FileUtil.GetProjectRelativePath( fullPath );
+            var instance     = ScriptableObject.CreateInstance( classType );
+
+            AssetDatabase.CreateAsset( instance, relativePath );
+            AssetDatabase.Refresh();
+        }
+    }
+}
--- README.md
+++ README.md
@@ -1,1 +1,3 @@
-# Kogane.ScriptableObjectCreatorOnInspector
+# Kogane Scriptable Object Creator On Inspector
+
+ScriptableObject を作成するボタンを Inspector に追加するエディタ拡張
